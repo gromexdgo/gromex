ba2feff5f5c7fab3ac04fa47b59d4450f0b8fe70
gromexdgo <sistemas@gromexdgo.com>
2014-10-13 14:50:21 -0500

    all files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GROMEX::Application.config.secret_token = '0406369edf20f7c6c9dcabed215a78723f9c8dd2c65fcf76d2e54ad025cc2109ef4b6299f47251b9b8abd2d77ece5bc4f7e9b79a00fe9653f1608917e40357a5'
